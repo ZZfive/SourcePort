c736785b1c66465eb300152dca147f159c0345c2
zjy <626958258@qq.com>
2026-07-18 22:35:03 +0800

    feat(core): add source capability registry

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
+export * from "./adapter.js";
 export * from "./contracts.js";
 export * from "./invariants.js";
+export * from "./registry.js";
 export * from "./schemas.js";
 export * from "./validate.js";
